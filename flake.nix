957a5fb5ba4904364e139e135133d143e4c47575
master_74 <bayev.v@gmail.com>
2026-01-13 13:07:19 +0500

    NixOS first commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+    description = "Hyprland on NixOS";
+
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
+	home-manager = {
+	    url = "github:nix-community/home-manager";
+	    inputs.nixpkgs.follows = "nixpkgs";
+
+	};
+    };
+
+    outputs = { nixpkgs, home-manager, ... }: {
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+	    system = "x86_64-linux";
+	    modules = [
+	        ./configuration.nix
+		home-manager.nixosModules.home-manager
+		{
+		    home-manager = {
+			useGlobalPkgs = true;
+			useUserPackages = true;
+			users.master = import ./home.nix;
+			backupFileExtension = "backup";		    
+    		    };
+		}
+	    ];
+        };	
+    };
+}
---
+++ home.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+   home.username = "master";
+   home.homeDirectory ="/home/master";
+   home.stateVersion = "25.11";
+
+   programs.bash = {
+        enable = true;
+        shellAliases = {
+	    btw = "echo i use hypland!";
+	    nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos";
+        };
+	profileExtra = ''
+	    if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTRN" = 1 ]; then
+	        exec uwsm start -S hyprland-uwsm.desktop
+	    fi
+	'';
+   };    
+}
